ca0c3f853f4a07ce4de694203eea84069d9acfc4
angenoir2468 <julie.parassouramanaik@protonmail.com>
2020-04-09 23:27:26 +0400

    tests ruby

---
+++ tests_ruby/lib/00_journalists.rb
@@ -0,0 +1,80 @@
+handle_list = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+#1 - Combien y a-t-il de journalistes dans ce array ?
+#2 - Combien d'handle contiennent un numéro ?
+#3 - Combien d'handle contiennent les 4 lettres du prénom "Aude" à la suite (sans prendre en compte les majuscules) ?
+#4 - Combien commencent par une majuscule (première lettre juste après le @) ?
+#5 - Combien contiennent au moins une majuscule ?
+#6 - Combien y a-t-il de underscore _ dans tous les pseudos confondus ?
+#7 - Trie la liste de handle par ordre alphabétique.
+#8 - Quels sont les 50 handles les plus courts de ce array ?
+#9 - Quelle est la position dans l'array de la personne @epenser ?
+
+#1 - Combien y a-t-il de journalistes dans ce array ?
+def numbers_of_journalist(handle_list)
+  puts "Il y a #{handle_list.length} dans cette liste."
+end
+
+#2 - Combien d'handle contiennent un numéro ?
+def contains_numbers(handle_list)
+  numbers_array = handle_list.grep(/0|1|2|3|4|5|6|7|8|9/)
+  puts "Il y a #{numbers_array.length} pseudonymes qui contiennent un numéro."
+end
+
+
+#3 - Combien d'handle contiennent les 4 lettres du prénom "Aude" à la suite (sans prendre en compte les majuscules) ?
+def contains_aude(handle_list)
+  array_aude = handle_list.grep(/aude/i)
+  puts "Il y a #{array_aude.length} qui contienent les 4 lettres du prénom aude à la suite."
+end
+
+#4 - Combien commencent par une majuscule (première lettre juste après le @) ?
+def start_uppercase(handle_list)
+  uppercase_array = handle_list.grep(/^@[A-Z]/)
+  puts "Il y a #{uppercase_array.length} qui commencent par une majuscule"
+end
+
+#5 - Combien contiennent au moins une majuscule ?
+def contains_uppercase(handle_list)
+  one_uppercase_array = handle_list.grep(/[A-Z]+/)
+  puts "Il y a #{one_uppercase_array.length} qui contiennent au moins une majuscule"
+end
+
+#6 - Combien y a-t-il de underscore _ dans tous les pseudos confondus ?
+def contains_underscore(handle_list)
+  array_underscore = handle_list.grep(/_/)
+  puts "Il y a #{array_underscore.length} underscore dans cette liste"
+end
+
+#7 - Trie la liste de handle par ordre alphabétique.
+def handle_croissant(handle_list)
+  handle_croissant = handle_list.sort
+  puts "La liste classée par ordre aphlabétique est : #{handle_croissant}"
+end
+
+#8 - Quels sont les 50 handles les plus courts de ce array ?
+def shorter_handle(handle_list)
+  shorter_handle_array = handle_list.sort_by{|handle| handle.length}
+  print "Les 50 pseudonymes les plus courts sont #{shorter_handle_array[0...50]}  "
+end
+
+
+#9 - Quelle est la position dans l'array de la personne @epenser ?
+def epenser_position(handle_list)
+puts "La position du pseudonyme @epenser est : #{handle_list.index ("@epenser")}"
+end
+
+
+def perform(handle_list)
+  numbers_of_journalist(handle_list)
+  contains_numbers(handle_list)
+  contains_aude(handle_list)
+  start_uppercase(handle_list)
+  contains_uppercase(handle_list)
+  contains_underscore(handle_list)
+  handle_croissant(handle_list)
+  shorter_handle(handle_list)
+  epenser_position(handle_list)
+end
+
+perform(handle_list)
